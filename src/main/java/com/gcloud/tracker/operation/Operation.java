4b0b136634de6c745d50505680f3d5ed03455c2e
Alexey Semenov <aurum25@ya.ru>
2021-08-26 13:53:12 +0300

    delete Operation

--- src/main/java/com/gcloud/tracker/operation/Operation.java
+++
@@ -1,9 +0,0 @@
-package com.gcloud.tracker.operation;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Operation {
-        void doGet(HttpServletRequest request, HttpServletResponse response) throws Exception;
-        void doPost(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
